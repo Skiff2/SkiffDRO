1ed0083ae2dd881447161d48640d11081c765836
Александр <47852517+Skiff2@users.noreply.github.com>
2022-07-20 21:47:58 +0300

    Обновление от 20,07,2022

--- Lathe/Lathe.ino
+++ Lathe/Lathe.ino
@@ -7,7 +7,8 @@ bool  IsBy = false;
 byte nn = 0;
 
 void setup() {
-Serial1.begin(9600); //А в идеале, настроить BT модуль на 115200 и тут тоже поставить 115200
+Serial.begin(115200); //Это не трогать! 
+Serial1.begin(9600); //А это, в идеале, настроить BT модуль на 115200 и тут тоже поставить 115200
   pinMode(PA0, INPUT_PULLDOWN);
   pinMode(PA1, INPUT_PULLDOWN);
   pinMode(PA2, INPUT_PULLDOWN);
@@ -38,10 +39,13 @@ void loop() {
   Serial1.print(ValX-500000);
   Serial1.print("|");
   Serial1.println(ValY-500000);
+  Serial.print(ValX-500000);
+  Serial.print("|");
+  Serial.println(ValY-500000);
 }
 
-void intAx() { IsAx = !IsAx;  if ((IsAx && !IsBx) || (!IsAx && IsBx)) ValX++; else ValX--; }
-void intBx() { IsBx = !IsBx;  if ((!IsAx && IsBx) || (IsAx && !IsBx)) ValX--; else ValX++;  }
+void intAx() { if (digitalRead(PA0) == IsAx) return; IsAx = !IsAx;  if ((IsAx && !IsBx) || (!IsAx && IsBx)) ValX++; else ValX--; }
+void intBx() { if (digitalRead(PA1) == IsBx) return;  IsBx = !IsBx;  if ((!IsAx && IsBx) || (IsAx && !IsBx)) ValX--; else ValX++;  }
 
-void intAy() { IsAy = !IsAy;  if ((IsAy && !IsBy) || (!IsAy && IsBy)) ValY++; else ValY--; }
-void intBy() { IsBy = !IsBy;  if ((!IsAy && IsBy) || (IsAy && !IsBy)) ValY--; else ValY++;  }
+void intAy() { if (digitalRead(PA2) == IsAy) return;  IsAy = !IsAy;  if ((IsAy && !IsBy) || (!IsAy && IsBy)) ValY++; else ValY--; }
+void intBy() { if (digitalRead(PA3) == IsBy) return;  IsBy = !IsBy;  if ((!IsAy && IsBy) || (IsAy && !IsBy)) ValY--; else ValY++;  }
--- Milling/Milling.ino
+++ Milling/Milling.ino
@@ -9,7 +9,8 @@ bool  IsAz = false;
 bool  IsBz = false;
 
 void setup() {
-Serial1.begin(9600); //А в идеале, настроить BT модуль на 115200 и тут тоже поставить 115200
+Serial.begin(115200); //это не трогать
+Serial1.begin(9600); //А это, в идеале, настроить BT модуль на 115200 и тут тоже поставить 115200
   pinMode(PA0, INPUT_PULLDOWN);
   pinMode(PA1, INPUT_PULLDOWN);
   pinMode(PA2, INPUT_PULLDOWN);
@@ -38,13 +39,18 @@ void loop() {
   Serial1.print(ValY-500000);
   Serial1.print("*");
   Serial1.println(ValZ-500000);
+  Serial.print(ValX-500000);
+  Serial.print("*");
+  Serial.print(ValY-500000);
+  Serial.print("*");
+  Serial.println(ValZ-500000);
 }
 
-void intAx() { IsAx = !IsAx;  if ((IsAx && !IsBx) || (!IsAx && IsBx)) ValX++; else ValX--; }
-void intBx() { IsBx = !IsBx;  if ((!IsAx && IsBx) || (IsAx && !IsBx)) ValX--; else ValX++;  }
+void intAx() {if (digitalRead(PA0) == IsAx) return; IsAx = !IsAx;  if ((IsAx && !IsBx) || (!IsAx && IsBx)) ValX++; else ValX--; }
+void intBx() {if (digitalRead(PA1) == IsBx) return; IsBx = !IsBx;  if ((!IsAx && IsBx) || (IsAx && !IsBx)) ValX--; else ValX++;  }
 
-void intAy() { IsAy = !IsAy;  if ((IsAy && !IsBy) || (!IsAy && IsBy)) ValY--; else ValY++; }
-void intBy() { IsBy = !IsBy;  if ((!IsAy && IsBy) || (IsAy && !IsBy)) ValY++; else ValY--;  }
+void intAy() {if (digitalRead(PA2) == IsAy) return; IsAy = !IsAy;  if ((IsAy && !IsBy) || (!IsAy && IsBy)) ValY--; else ValY++; }
+void intBy() {if (digitalRead(PA3) == IsBy) return; IsBy = !IsBy;  if ((!IsAy && IsBy) || (IsAy && !IsBy)) ValY++; else ValY--;  }
 
-void intAz() { IsAz = !IsAz;  if ((IsAz && !IsBz) || (!IsAz && IsBz)) ValZ++; else ValZ--; }
-void intBz() { IsBz = !IsBz;  if ((!IsAz && IsBz) || (IsAz && !IsBz)) ValZ--; else ValZ++;  }
+void intAz() {if (digitalRead(PA4) == IsAz) return; IsAz = !IsAz;  if ((IsAz && !IsBz) || (!IsAz && IsBz)) ValZ++; else ValZ--; }
+void intBz() {if (digitalRead(PA5) == IsBz) return; IsBz = !IsBz;  if ((!IsAz && IsBz) || (IsAz && !IsBz)) ValZ--; else ValZ++;  }
